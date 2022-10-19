a95459611fad434b7bb048eeccb0eaa51a809426
bdom <dberre@free.fr>
2022-10-19 21:54:21 +0200

    Minor change. The name of the photo is provided to the camera

--- app/src/main/java/com/bdomperso/ecsfloralies/CameraCaptureActivity.kt
+++ app/src/main/java/com/bdomperso/ecsfloralies/CameraCaptureActivity.kt
@@ -1,18 +1,35 @@
 package com.bdomperso.ecsfloralies
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.bdomperso.ecsfloralies.fragments.CameraFragment
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraCaptureActivity : AppCompatActivity() {
+
+    private lateinit var photoFile: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_capture)
 
+        photoFile = File(MainActivity.getOutputDirectory(this), SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS", Locale.US)
+            .format(System.currentTimeMillis()) + ".jpg")
 
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                val bundle = Bundle()
+                bundle.putSerializable("photoFile", photoFile)
+                setReorderingAllowed(true)
+                add<CameraFragment>(R.id.fragmentContainerView, "", bundle)
+            }
+        }
 
         findViewById<Button>(R.id.buttonCloseCameraCapture).setOnClickListener {
             saveCapture()
@@ -20,9 +37,8 @@ class CameraCaptureActivity : AppCompatActivity() {
     }
 
     private fun saveCapture() {
-        val file = File("")
         val intent = Intent(this, SaveCaptureActivity::class.java)
-        intent.putExtra("image_path", file.absolutePath)
+        intent.putExtra("image_path", photoFile.absolutePath)
         this.startActivity(intent)
     }
 }
