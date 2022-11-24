a8786a8d55df7fc38837fc177d5cedc6b994d100
dberre <74907040+dberre@users.noreply.github.com>
2022-11-24 22:09:07 +0100

    Add a test to check a regular expression

--- app/src/test/java/com/bdomperso/ecsfloralies/ExampleUnitTest.kt
+++ app/src/test/java/com/bdomperso/ecsfloralies/ExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.bdomperso.ecsfloralies
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.File
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,29 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun filename_regex() {
+        parseFilename(File("A_ET4_F2_FACE_ASC_SDB.jpg"))
+        parseFilename(File("C_ET1_F2_MUR_ASC_CUI.jpg"))
+    }
+
+    private fun parseFilename(file: File) {
+//        val pattern = "([ABC])_ET([01234])_(.*)_(.*)"
+        val pattern = "([ABC])_ET([01234])_(.*)_(.*)\\."
+        val matches = Regex(pattern).find(file.name)
+        if (matches != null) {
+            if (matches!!.groups.isNotEmpty()) {
+                println("count: ${matches.groups.count()}")
+                for(group in matches!!.groups) {
+                    println(group)
+                }
+            } else {
+                println("no matches")
+            }
+        } else {
+            println("pattern no found")
+        }
     }
 }
